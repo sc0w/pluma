d2e8157f31a3b77e5c8e0a0a60fcb318a69f7403
Pablo Barciela <scow@riseup.net>
2018-01-22 15:09:15 +0100

    pluma-file-browser-plugin.c gtkstock

--- plugins/filebrowser/pluma-file-browser-plugin.c
+++ plugins/filebrowser/pluma-file-browser-plugin.c
@@ -574,7 +574,7 @@ on_selection_changed_cb (GtkTreeSelection *selection,
 
 static GtkActionEntry extra_actions[] =
 {
-	{"SetActiveRoot", GTK_STOCK_JUMP_TO, N_("_Set root to active document"),
+	{"SetActiveRoot", "go-jump", N_("_Set root to active document"),
 	 NULL,
 	 N_("Set the root to the active document location"),
 	 G_CALLBACK (on_action_set_active_root)}
@@ -717,7 +717,7 @@ pluma_file_browser_plugin_activate (PeasActivatable *activatable)
 		image = gtk_image_new_from_pixbuf(pixbuf);
 		g_object_unref(pixbuf);
 	} else {
-		image = gtk_image_new_from_stock(GTK_STOCK_INDEX, GTK_ICON_SIZE_MENU);
+		image = gtk_image_new_from_icon_name("gtk-index", GTK_ICON_SIZE_MENU);
 	}
 
 	gtk_widget_show(image);
